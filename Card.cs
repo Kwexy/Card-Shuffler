aec8d8cc8c4e0f7f21ad794e46d3a9c89ec44c56
Kwexy <31973290+Kwexy@users.noreply.github.com>
2022-08-02 20:04:40 -0700

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Shuffler {
+    public class Card {
+        public enum Suite {Spade = 1, Club = 2, Heart = 3, Diamond = 4};
+        public enum Value {Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13}
+
+        private Suite suite;
+        private Value value;
+
+        public Card(Suite s, Value v) {
+            suite = s;
+            value = v;
+        }
+
+        public Suite getsuite() {
+            return suite;
+        }
+
+        public Value getValue() {
+            return value;
+        }
+
+        public static string getCardName(Card c) {
+            return c.value + " of " + c.suite + "s.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+namespace Card_Shuffler {
+    public class Program {
+
+        public static List<Card> deck = new List<Card>();
+        public static List<Card> tempShuffleDeck = new List<Card>();
+
+        public Program() {
+            populateDeck(deck);
+            shuffle();
+            printDeck();
+            
+            }
+
+        public void populateDeck(List<Card> list) {
+            for (int s = 1; s <= 4; s++) {
+                for (int v = 1; v <= 13; v++) {
+                    deck.Add(new Card((Card.Suite)s, (Card.Value)v));
+                    //Console.WriteLine();
+                }
+            }
+        }
+
+        public void printDeck () {
+            for (int i = 0; i < 52; i++) {
+                switch(deck[i].getsuite()) {
+                    case Card.Suite.Spade:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case Card.Suite.Club:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Card.Suite.Heart:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Card.Suite.Diamond:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    default: Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                }
+
+             
+                Console.WriteLine(Card.getCardName(deck[i]));
+            }
+        } 
+
+        public void shuffle() {
+            Random rand = new Random();
+            for (int deckSize = 52; deckSize > 0; deckSize--) {
+                int r = rand.Next(deckSize);
+                tempShuffleDeck.Add(deck[r]);
+                deck.RemoveAt(r);
+            }
+            deck = tempShuffleDeck;
+        }
+
+        public static void Main(string[] args) {
+            new Program();
+        }
+    }
+}
